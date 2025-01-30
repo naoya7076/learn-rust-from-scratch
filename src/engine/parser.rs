df34c482528b5fb252c24f57127c0b5d22e48194
naoya7076 <naoya7076@gmail.com>
2025-01-30 23:41:53 +0900

    エラー定義のASTの構造体を実装

---
+++ src/engine/parser.rs
@@ -0,0 +1,25 @@
+//! 正規表現の式をパースし、ASTに変換する
+use std::{
+    error::Error,
+    fmt::{self, Display},
+    mem::take,
+};
+
+#[derive(Debug)]
+pub enum AST {
+    Char(char),
+    Plus(Box<AST>),
+    Star(Box<AST>),
+    Question(Box<AST>),
+    Or(Box<AST>, Box<AST>),
+    Seq(Vec<AST>),
+}
+
+#[derive(Debug)]
+pub enum ParseError {
+    InvalidEscape(usize, char),
+    InvalidRightParen(usize),
+    NoPrev(usize),
+    NoRightParen,
+    Empty,
+}
