21d0b146a765e18ebd9d6e9247990e694b34b91a
naoya7076 <naoya7076@gmail.com>
2025-02-03 12:13:54 +0900

    codegenを修正

--- src/engine/codegen.rs
+++ src/engine/codegen.rs
@@ -1,5 +1,5 @@
 use super::{parser::AST, Instruction};
-use create::helper::safe_add;
+use crate::helper::safe_add;
 use std::{
     error::Error,
     fmt::{self, Display},
@@ -15,7 +15,7 @@ pub enum CodeGenError {
 
 impl Display for CodeGenError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Code Generation Error: {:?}", self)?
+        write!(f, "Code Generation Error: {:?}", self)
     }
 }
 
@@ -27,78 +27,86 @@ struct Generator {
     insts: Vec<Instruction>,
 }
 
-fn inc_pc(&mut self) -> Result<(), CodeGenError> {
-    safe_add(&mut self.pc, &1, || CodeGenError::POverFlow)
-}
-
-fn gen_expr(&mut self, ast: &AST) -> Result<(), CodeGenError> {
-    match ast {
-        AST::Char(c) => self.gen_char(*c)?,
-        AST::Or(e1, e2) => self.gen_or(e1, e2)?,
-        AST::Plus(e) => self.gen_plus(e)?,
-        AST::Star(e) => self.gen_star(e)?,
-        AST::Question(e) => self.gen_question(e)?,
-        AST::Seq(v) => self.gen_seq(v)?,
+impl Default for Generator {
+    fn default() -> Self {
+        Self {
+            pc: 0,
+            insts: Vec::new(),
+        }
     }
-    Ok(())
 }
-
-fn gen_seq(&mut self, exprs: &[AST]) -> Result<(), CodeGenError> {
-    for e in exprs {
-        self.gen_expr(e)?;
-    }
-    Ok(())
-}
-
-fn gen_char(&mut self, c: char) -> Result<(), CodeGenError> {
-    let inst = Instruction::Char(c);
-    self.insts.push(inst);
-    self.inc_pc()?;
-    Ok(())
-}
-/// ```text
-///     split L1, L2
-/// L1: e1のコード
-/// L2: e2のコード
-/// L3:
-/// ```
-fn gen_or(&mut self, e1: &AST, e2: &AST) -> Result<(), CodeGenError> {
-    let split_addr = self.pc;
-    self.inc_pc()?;
-
-    let split = Instruction::Split(self.pc, 0);
-    self.insts.push(split);
-
-    self.gen_expr(e1)?;
-
-    let jmp_addr = self.pc;
-    self.insts.push(Instruction::Jump(0));
-
-    self.inc_pc()?;
-    if let Some(Instruction::Split(_, l2)) = self.insts.get_mut(split_addr) {
-        *l2 = self.pc;
-    } else {
-        return Err(Box::new(CodeGenError::FailOr));
+impl Generator {
+    fn inc_pc(&mut self) -> Result<(), CodeGenError> {
+        safe_add(&mut self.pc, &1, || CodeGenError::PCOverFlow)
     }
 
-    self.gen_expr(e2)?;
-
-    if let Some(Instruction::Jump(l3)) = self.insts.get_mut(jmp_addr) {
-        *l3 = self.pc;
-    } else {
-        return Err(Box::new(CodeGenError::FailOr));
+    fn gen_expr(&mut self, ast: &AST) -> Result<(), CodeGenError> {
+        match ast {
+            AST::Char(c) => self.gen_char(*c)?,
+            AST::Or(e1, e2) => self.gen_or(e1, e2)?,
+            AST::Seq(v) => self.gen_seq(v)?,
+            _ => unimplemented!(),
+        }
+        Ok(())
     }
-    Ok(())
+
+    fn gen_seq(&mut self, exprs: &[AST]) -> Result<(), CodeGenError> {
+        for e in exprs {
+            self.gen_expr(e)?;
+        }
+        Ok(())
+    }
+
+    fn gen_char(&mut self, c: char) -> Result<(), CodeGenError> {
+        let inst = Instruction::Char(c);
+        self.insts.push(inst);
+        self.inc_pc()?;
+        Ok(())
+    }
+    /// ```text
+    ///     split L1, L2
+    /// L1: e1のコード
+    /// L2: e2のコード
+    /// L3:
+    /// ```
+    fn gen_or(&mut self, e1: &AST, e2: &AST) -> Result<(), CodeGenError> {
+        let split_addr = self.pc;
+        self.inc_pc()?;
+
+        let split = Instruction::Split(self.pc, 0);
+        self.insts.push(split);
+
+        self.gen_expr(e1)?;
+
+        let jmp_addr = self.pc;
+        self.insts.push(Instruction::Jump(0));
+
+        self.inc_pc()?;
+        if let Some(Instruction::Split(_, l2)) = self.insts.get_mut(split_addr) {
+            *l2 = self.pc;
+        } else {
+            return Err(CodeGenError::FailOr);
+        }
+
+        self.gen_expr(e2)?;
+
+        if let Some(Instruction::Jump(l3)) = self.insts.get_mut(jmp_addr) {
+            *l3 = self.pc;
+        } else {
+            return Err(CodeGenError::FailOr);
+        }
+        Ok(())
+    }
+
+    fn gen_code(&mut self, ast: &AST) -> Result<(), CodeGenError> {
+        self.gen_expr(ast)?;
+        self.inc_pc()?;
+        self.insts.push(Instruction::Match);
+        Ok(())
+    }
 }
 
-fn gen_code(&mut self, ast: &AST) -> Result<(), CodeGenError> {
-    self.gen_expr(ast)?;
-    self.inc_pc;
-    self.insts.push(Instruction::Match);
-    Ok(())
-}
-
-pub fn codegen(ast: &AST) -> Result<Vec<Instruction>, CodeGenError> {
+pub fn get_code(ast: &AST) -> Result<Vec<Instruction>, CodeGenError> {
     let mut gen = Generator::default();
     gen.gen_code(ast)?;
     Ok(gen.insts)
